99579976bf955de3a0a2de992a745da7d9bcf7ed
Julian Saenz <erasedot@gmail.com>
2017-02-01 16:55:20 -0500

    Add parallax

--- public/javascripts/src/containers/Homepage.jsx
+++ public/javascripts/src/containers/Homepage.jsx
@@ -23,7 +23,7 @@ class Homepage extends React.Component {
         </nav>
         <section className="section container-1">
           <div className="section-content">
-            <div className="first-text-content">
+            <div className="first-text-content rellax" data-rellax-speed="-2">
               <h2 className="item" data-aos="fade-up" data-aos-duration="2000">Clear our Water or</h2>
               <h1 className="item" data-aos="fade-up" data-aos-duration="1000">Find Our Girls</h1>
               <p data-aos="fade-down" data-aos-duration="1000">
@@ -37,7 +37,7 @@ class Homepage extends React.Component {
           </div>
         </section>
         <section className="section container-2">
-          <div className="section-content">
+          <div className="section-content rellax" data-rellax-speed="2">
             <div className="item circles" data-aos="fade-up-left">
               <div className="circle-part"></div>
               <p className="paragraph-part"> We don things</p>
